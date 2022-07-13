8282356d889c04aa80e7f032fd8b3572ceeb2900
Space_Fox <spacefox@FoxStarship>
2022-07-14 01:00:22 +0300

    Добавлены нечеткие непрерывные автоматы.

--- source/python/fuzzy_torch/modules/ffsa.py
+++ source/python/fuzzy_torch/modules/ffsa.py
@@ -87,3 +87,65 @@ class TimeIndependentFFSA(FuzzyFSA):
             new_activation = self.normalize_activation(new_activation)
 
         return new_activation
+
+
+
+class FuzzyTransitionContinuous(FuzzyTransition):
+    """
+    Структура для описания плавного нечеткого перехода.
+    """
+
+    def __init__(self, from_idx, to_idx, condition, speed=1.0):
+        super().__init__(from_idx=from_idx, to_idx=to_idx, condition=condition)
+        self.speed = torch.nn.Parameter(torch.ones(1) * speed)
+
+    def forward(self, *args):
+        return self.condition(*args)
+
+
+
+class TimeDependentFFSA(FuzzyFSA):
+    """
+    Зависящий от времени нечеткий конечный автомат.
+    """
+
+    def __init__(self, logic, normalize=False):
+        super().__init__(logic=logic, normalize=normalize)
+
+
+    def forward(self, input, activation, dt):
+        assert (activation.dim() == 2) and (activation.size()[1] == len(self.states))
+
+        from_activation = torch.zeros_like(activation)
+        to_activation   = torch.zeros_like(activation)
+
+        for transition in self.transitions:
+            # Не работает, так как используется in-place срезы (нарушает дерево производных).
+            #condition = transition.condition(input).squeeze()
+            #from_activations[:, transition.from_idx] = self.logic.fuzzy_or(from_activation[:, transition.from_idx], condition)
+            #to_activations[:, transition.to_idx] = self.logic.fuzzy_or(to_activation[:, transition.to_idx], condition)
+
+            # Условие перехода, размноженное на все состояния.
+            # Конъюнкция активности состояния, из которого происходит переход, и условия самого перехода.
+            condition = self.logic.fuzzy_and(activation[:, transition.from_idx, None], transition(input)).repeat(1, len(self.states))
+            speed = condition * transition.speed
+
+            # Вспомогательные тензоры-множители.
+            helper_from = torch.zeros_like(condition)
+            helper_from[:, transition.from_idx] = 1
+
+            helper_to = torch.zeros_like(condition)
+            helper_to[:, transition.to_idx] = 1
+
+            from_activation += helper_from * speed
+            to_activation += helper_to * speed
+
+
+        derivative = to_activation - from_activation
+
+        new_activation = torch.clip(activation + derivative * dt, 0, 1)
+
+        if self.normalize:
+            new_activation = self.normalize_activation(new_activation)
+
+        return new_activation
